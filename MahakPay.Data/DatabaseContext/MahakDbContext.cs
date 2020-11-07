134d42b293df001c350491c23e5516175fff0950
persiamahak@gmail.com <persiamahak@gmail.com>
2020-11-07 10:49:27 +0330

    S04-P01_UserModel

---
+++ MahakPay.Data/DatabaseContext/MahakDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MahakPay.Data.DatabaseContext
+{
+    class MahakDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        {
+            optionBuilder.UseSqlServer(@"Data Source=MAHAK\SQLINST01;Initial Catalog=MahakPaydb;Integrated Security=True;MultipleActiveResultSets=True;");
+        }
+    }
+}
